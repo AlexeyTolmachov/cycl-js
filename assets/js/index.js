cda2f6ee6c47feba89cc6147b800f32396a77d9d
AlexeyTolmachov <taar12sh@gmail.com>
2021-07-26 10:57:04 +0300

    add cicl

---
+++ assets/js/index.js
@@ -0,0 +1,54 @@
+// for (let i = 25; i >= 0; i--) {
+//     let a = i;
+//   console.log(a);
+// }
+
+// let i = 25;
+// while (i >= 0) {
+//     let a = i;
+//   console.log(a);
+//   i--;
+// }
+
+// let j = 25;
+// do {
+//   let a = j;
+//   console.log(a);
+//   j--;
+// } while (j >= 0);
+
+// for (let index = 10; index <= 50; index++) {
+//   if (index % 5 === 0) {
+//     let a = index;
+//     console.log(a);
+//   }
+// }
+
+// let i = 10;
+// while (i <= 50) {
+//   i++;
+//   if (a % 5 === 0) {
+//     let a = i;
+//     console.log(a);
+//   }
+// }
+
+// let i = 10;
+// do {
+//   let a = i;
+//   i++;
+//   if (a % 5 === 0) {
+//     console.log(a);
+//   }
+// } while (i <= 50);
+
+
+// const calc = function (number) {
+//     while (number !== 6) {
+//         number = +prompt("enter answer 2 + 2 * 2")
+//         continue;
+//     } 
+//     alert("yes 2 + 2 * 2 = 6 ");
+    
+// } 
+
